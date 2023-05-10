617b0fd8b50c93a6005386e58d83d3b9e4edcca9
Boxfriend <cawills394@gmail.com>
2023-05-10 02:16:38 -0400

    Add project files.

---
+++ SpaceTrader/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceTrader;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SpaceTrader/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SpaceTrader;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
