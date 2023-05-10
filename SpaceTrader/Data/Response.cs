7202f6fd7e22af12100be4683717b0617d14c3f5
Boxfriend <cawills394@gmail.com>
2023-05-10 03:32:24 -0400

    WIP setting up super basic agent adding/registration

---
+++ SpaceTrader/Data/Response.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpaceTrader.Data;
+internal class Response<T> where T : ApiObject
+{
+    [JsonPropertyName("data")] public T Data { get; set; }
+    [JsonPropertyName("meta")] public Meta Meta { get; set; }
+}
+
+internal record Meta(int Total, int Page, int Limit);
+
+internal abstract record ApiObject();
+
+internal record Agent([property:JsonPropertyName("accountId")]string AccountID, [property: JsonPropertyName("symbol")] string Name, 
+    [property: JsonPropertyName("headquarters")] string Headquarters, [property: JsonPropertyName("credits")] int Credits) : ApiObject;
