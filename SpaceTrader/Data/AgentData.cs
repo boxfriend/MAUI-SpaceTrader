de9beb2a6b5336381bbdcbd3c2e4bace5e6f4cc6
Boxfriend <cawills394@gmail.com>
2023-05-11 02:19:06 -0400

    split AgentData to its own file, added method to convert from API response object to DB object

---
+++ SpaceTrader/Data/AgentData.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace SpaceTrader.Data;
+
+public class AgentData
+{
+    [PrimaryKey,Unique] public string AccountID { get; set; }
+    [Unique] public string Name { get; set; }
+    [Unique] public string Token { get; set; }
+    public int Credits { get; set; }
+
+    internal static AgentData FromAPIObject(Agent data, string token)
+    {
+        return new AgentData()
+        {
+            AccountID = data.AccountID,
+            Name = data.Name,
+            Token = token,
+            Credits = data.Credits
+        };
+    }
+}
