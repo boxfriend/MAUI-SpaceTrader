677161af8af02375b405afd57bf7839e614a27f5
Boxfriend <cawills394@gmail.com>
2023-05-13 03:12:17 -0400

    Added Serilog for logging

--- SpaceTrader/MauiProgram.cs
+++ SpaceTrader/MauiProgram.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.Json;
+﻿using System.IO;
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using RestSharp;
 using RestSharp.Serializers.Json;
+using Serilog;
 using SpaceTrader.Data;
 
 namespace SpaceTrader;
@@ -24,14 +26,36 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
+		var envPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MAUI-SpaceTraders");
+		Directory.CreateDirectory(envPath);
 
-		var clientOptions = new RestClientOptions(@"https://api.spacetraders.io/v2");
+		InitLogger(envPath);
+
+        var clientOptions = new RestClientOptions(@"https://api.spacetraders.io/v2");
 		var serializeOptions = new JsonSerializerOptions(new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
         builder.Services.AddSingleton(s => new RestClient(clientOptions,configureSerialization: s => s.UseSystemTextJson(serializeOptions)));
 
-		var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Agents.db");
+		var path = Path.Combine(envPath, "Agents.db");
 		builder.Services.AddSingleton(s => ActivatorUtilities.CreateInstance<AgentDbController>(s, path));
 
 		return builder.Build();
 	}
+
+    private static void InitLogger (string path)
+    {
+		const string template = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] ({ThreadId}) {Message:lj}{NewLine}{Exception}";
+
+        Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Verbose()
+			.WriteTo.Async(a => a.Console(outputTemplate: template))
+#else
+            .MinimumLevel.Information()
+#endif
+            .WriteTo.Async(a => a.File(Path.Combine(path, "log.txt"),
+                outputTemplate: template))
+            .CreateLogger();
+
+        Log.Information("Serilog Initialized. Logging started.");
+    }
 }
