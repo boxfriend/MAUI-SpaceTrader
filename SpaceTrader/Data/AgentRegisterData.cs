b3a55011865b58e79952f52edf0bc723ec11e853
Boxfriend <cawills394@gmail.com>
2023-05-14 20:09:09 -0400

    made agent registration better

---
+++ SpaceTrader/Data/AgentRegisterData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaceTrader.Data;
+
+public class AgentRegisterData
+{
+    [Required,StringLength(14,MinimumLength = 3, ErrorMessage = "Agent Name must be between 3 and 14 characters.")]
+    public string AgentName { get; set; }
+
+    [Required]
+    public string FactionName { get; set; }
+
+    public static bool ValidateFaction(string FactionName)
+    {
+        return FactionName.ToUpper() is "COSMIC" or "VOID" or "GALACTIC" or "QUANTUM" or "DOMINION";
+    }
+}
