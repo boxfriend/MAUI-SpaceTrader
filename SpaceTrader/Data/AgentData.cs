527d44b005aef55688aa7440c7c3c434b4a556f1
Boxfriend <cawills394@gmail.com>
2023-05-21 16:37:45 -0400

    agent data will now include agent's HQ

--- SpaceTrader/Data/AgentData.cs
+++ SpaceTrader/Data/AgentData.cs
@@ -7,6 +7,7 @@ public class AgentData
     [PrimaryKey,Unique] public string AccountID { get; set; }
     [Unique] public string Name { get; set; }
     [Unique] public string Token { get; set; }
+    public string Headquarters { get; set; }
     public int Credits { get; set; }
 
     internal static AgentData FromAPIAgent(Agent data, string token)
@@ -16,7 +17,8 @@ public class AgentData
             AccountID = data.AccountID,
             Name = data.Name,
             Token = token,
-            Credits = data.Credits
+            Credits = data.Credits,
+            Headquarters = data.Headquarters,
         };
     }
 }
