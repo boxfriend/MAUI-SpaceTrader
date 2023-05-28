893a7ad44300c00f6a339304602b4c6b6e19b400
Boxfriend <cawills394@gmail.com>
2023-05-28 11:31:38 -0400

    added ship database table with relationship to AgentData

---
+++ SpaceTrader/Data/Database/ShipData.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace SpaceTrader.Data;
+
+public class ShipData
+{
+    [PrimaryKey,Unique] public string Symbol { get; set; }
+    [ForeignKey(typeof(AgentData))] public string AccountID { get; set; }
+    public Ship Ship { get; set; }
+}
---
+++ SpaceTrader/Data/Database/ShipDbController.cs
@@ -0,0 +1,66 @@
+﻿using SQLite;
+
+namespace SpaceTrader.Data;
+
+internal class ShipDbController
+{
+    private readonly string _dbPath;
+    private SQLiteAsyncConnection _connection;
+
+    public ShipDbController (string path)
+    {
+        _dbPath = path;
+    }
+
+    public async Task InitializeAsync ()
+    {
+
+        if (_connection != null)
+            return;
+
+        _connection = new(_dbPath);
+
+        await _connection.CreateTableAsync<ShipData>();
+    }
+
+    public async Task InsertOrUpdate(ShipData shipData)
+    {
+        await InitializeAsync();
+        await _connection.InsertOrReplaceAsync(shipData);
+    }
+
+    public async Task InsertOrUpdate(List<ShipData> shipDatas)
+    {
+        foreach(var ship in shipDatas)
+            await InsertOrUpdate(ship);
+    }
+
+    public async Task Delete (ShipData data)
+    {
+        await InitializeAsync();
+        await _connection.DeleteAsync(data);
+    }
+
+    public async Task DeleteAllFromAgent(AgentData data)
+    {
+        var allShips = await GetAgentShips(data);
+        foreach(var ship in allShips)
+        {
+            await Delete(ship);
+        }
+    }
+
+    public async Task<List<ShipData>> GetAgentShips (AgentData data) => await GetAgentShips(data.AccountID);
+    public async Task<List<ShipData>> GetAgentShips (string accountID)
+    {
+        await InitializeAsync();
+        return await _connection.Table<ShipData>().Where(ship => ship.AccountID == accountID).ToListAsync();
+    }
+
+    public async Task<ShipData> GetShip(string shipSymbol)
+    {
+        await InitializeAsync();
+        return await _connection.GetAsync<ShipData>(shipSymbol);
+    }
+    public async Task<ShipData> GetShip(ShipData ship) => await GetShip(ship.Symbol);
+}
