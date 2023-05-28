73456ec55913ac5480be3ba3f4455ce5e15b0382
Boxfriend <cawills394@gmail.com>
2023-05-28 12:06:57 -0400

    added method to create ShipData object from Ship API response WIP

--- SpaceTrader/Data/Database/ShipData.cs
+++ SpaceTrader/Data/Database/ShipData.cs
@@ -7,5 +7,16 @@ public class ShipData
 {
     [PrimaryKey,Unique] public string Symbol { get; set; }
     [ForeignKey(typeof(AgentData))] public string AccountID { get; set; }
-    public Ship Ship { get; set; }
+    
+    public string Role { get; set; }
+    public string SystemSymbol { get; set; }
+    public string WaypointSymbol { get; set; }
+
+    public static ShipData FromApiObject (Ship ship) => new()
+    {
+        Symbol = ship.Symbol,
+        Role = ship.Registration.Role,
+        SystemSymbol = ship.Nav.SystemSymbol,
+        WaypointSymbol = ship.Nav.WaypointSymbol,
+    };
 }
