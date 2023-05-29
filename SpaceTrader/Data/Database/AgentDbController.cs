25c3898871e0b50c0ba15b5ee42e848f804beba7
Boxfriend <cawills394@gmail.com>
2023-05-29 17:58:30 -0400

    implemented base class and removed contract related stuff from agentdbcontroller

--- SpaceTrader/Data/Database/AgentDbController.cs
+++ SpaceTrader/Data/Database/AgentDbController.cs
@@ -5,96 +5,26 @@ using SQLiteNetExtensions.Extensions;
 using SQLiteNetExtensionsAsync.Extensions;
 
 namespace SpaceTrader.Data;
-internal class AgentDbController
+internal class AgentDbController : BaseDbController
 {
-    private readonly string _dbPath;
-    private SQLiteAsyncConnection _connection;
+    public AgentDbController(string path, ApiClient client, ILogger<BaseDbController> logger) : base(path, client, logger) { }
 
-    private readonly ApiClient _client;
-    private readonly ILogger<AgentDbController> _logger;
-
-    public AgentDbController(string path, ApiClient client, ILogger<AgentDbController> logger)
+    protected override async Task Initialize()
     {
-        _dbPath = path;
-        _client = client;
-        _logger = logger;
-    }
+        if(_isInitialized) return;
 
-    public async Task InitializeAsync()
-    {
-        
-        if (_connection != null)
-            return;
-
-        _connection = new(_dbPath);
-
+        _isInitialized = true;
         await _connection.CreateTableAsync<Agent>();
-        await _connection.CreateTableAsync<Contract>();
-        await _connection.CreateTableAsync<ContractTerms>();
-        await _connection.CreateTableAsync<ContractPayment>();
-        await _connection.CreateTableAsync<ContractDeliverGood>();
-
     }
 
-    public async Task<List<Agent>> GetAll ()
+    public async Task Insert (Agent data)
     {
-        await InitializeAsync();
-        return await _connection.Table<Agent>().ToListAsync();
-    }
+        await Initialize();
+        await _connection.InsertOrReplaceWithChildrenAsync(data, true);
 
-    public async Task<Agent> Get(string accountID)
-    {
-        await InitializeAsync();
-        return await _connection.FindAsync<Agent>(accountID);
-    }
-    public async Task<Agent> Create (Agent data)
-    {
-        await InitializeAsync();
-        await _connection.InsertAsync(data);
-        return data;
-    }
-
-    public async Task Update (Agent agent)
-    {
-        await InitializeAsync();
-        var agentData = await Get(agent.AccountID);
-        await _connection.UpdateAsync(agentData);
-
-        if(_client.LoggedInAgent.AccountID == agentData.AccountID)
+        if (_client.LoggedInAgent.AccountID == data.AccountID)
         {
-            _client.Login(agentData);
-        }
-    }
-
-    public async Task Delete (Agent data) => await _connection.DeleteAsync(data);
-
-    public async Task InsertContracts(params Contract[] contracts)
-    {
-        await InitializeAsync();
-        foreach (var contract in contracts)
-        {
-            contract.Terms.ID = contract.ID;
-            contract.Terms.Payment.ID = contract.ID;
-
-            foreach (var good in contract.Terms.Deliveries)
-            {
-                good.TermsID = contract.ID;
-            }
-        }
-        await _connection.InsertOrReplaceAllWithChildrenAsync(contracts, true);
-    }
-
-    public async Task<Contract> GetContract(string contractID)
-    {
-        await InitializeAsync();
-        try
-        {
-            return await _connection.GetWithChildrenAsync<Contract>(contractID,true);
-        }
-        catch(Exception ex)
-        {
-            _logger.LogCritical(ex.Message);
-            throw;
+            _client.Login(data);
         }
     }
 }
---
+++ SpaceTrader/Data/Database/ContractDbController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using SQLiteNetExtensionsAsync.Extensions;
+
+namespace SpaceTrader.Data;
+
+internal class ContractDbController : BaseDbController
+{
+    public ContractDbController (string path, ApiClient client, ILogger<BaseDbController> logger) : base(path, client, logger) { }
+
+    protected override async Task Initialize ()
+    {
+        if (_isInitialized)
+            return;
+
+        _isInitialized = true;
+        await _connection.CreateTableAsync<Contract>();
+        await _connection.CreateTableAsync<ContractTerms>();
+        await _connection.CreateTableAsync<ContractPayment>();
+        await _connection.CreateTableAsync<ContractDeliverGood>();
+    }
+
+    public async Task<List<Contract>> GetAllFromAgent(Agent agent) => await GetAllFromAgent(agent.AccountID);
+    public async Task<List<Contract>> GetAllFromAgent(string agentID)
+    {
+        await Initialize();
+        return await _connection.GetAllWithChildrenAsync<Contract>(c => c.AccountID == agentID, true);
+    }
+
+    public async Task Insert (params Contract[] contracts)
+    {
+        await Initialize();
+        foreach (var contract in contracts)
+        {
+            contract.Terms.ID = contract.ID;
+            contract.Terms.Payment.ID = contract.ID;
+
+            foreach (var good in contract.Terms.Deliveries)
+            {
+                good.TermsID = contract.ID;
+            }
+        }
+        await _connection.InsertOrReplaceAllWithChildrenAsync(contracts, true);
+    }
+
+}
