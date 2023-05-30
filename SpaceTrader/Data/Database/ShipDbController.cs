40fb726c184709495d6d8fe33f748713df7591e1
Boxfriend <cawills394@gmail.com>
2023-05-29 20:22:10 -0400

    ship db now uses API response record instead of ShipData class, this added a dozen tables because i don't have a better idea of how to organize all this data

--- SpaceTrader/Data/Database/ShipDbController.cs
+++ SpaceTrader/Data/Database/ShipDbController.cs
@@ -6,43 +6,48 @@ namespace SpaceTrader.Data;
 
 internal class ShipDbController : BaseDbController
 {
-
-
     public ShipDbController (string path, ApiClient client, ILogger<BaseDbController> logger) : base(path, client, logger) { }
     protected override async Task Initialize ()
     {
-
-        if (_isInitialized)
-            return;
-
-        _isInitialized = true;
-        await _connection.CreateTableAsync<ShipData>();
-        await _connection.CreateTableAsync<CargoItem>();
+        await _connection.CreateTableAsync<Ship>();
+        await _connection.CreateTableAsync<ShipRegistration>();
+        await _connection.CreateTableAsync<ShipNav>();
+        await _connection.CreateTableAsync<ShipCrew>();
+        await _connection.CreateTableAsync<ShipFrame>();
+        await _connection.CreateTableAsync<ShipReactor>();
+        await _connection.CreateTableAsync<ShipEngine>();
+        await _connection.CreateTableAsync<ShipModule>();
+        await _connection.CreateTableAsync<ShipMount>();
+        await _connection.CreateTableAsync<ShipCargo>();
+        await _connection.CreateTableAsync<ShipCargoItem>();
+        await _connection.CreateTableAsync<ShipFuel>();
     }
 
-    
-    public async Task Insert(Ship ship, string accountID)
-    {
-        var shipData = ShipData.FromApiObject(ship);
-        shipData.AccountID = accountID;
-        await Insert(shipData);
-        await DeleteShipCargo(shipData);
-        foreach (var item in ship.Cargo.Inventory)
-        {
-            var cargo = CargoItem.FromApiObject(item);
-            cargo.Ship = shipData.Symbol;
-            await _connection.InsertOrReplaceAsync(cargo);
-        }
-    }
-    public async Task Insert (IEnumerable<Ship> ships, string accountID)
+    public async Task Insert (string accountID, params Ship[] ships)
     {
         foreach(var ship in ships)
         {
-            await Insert(ship, accountID);
+            ship.AccountID = accountID;
+            ship.Registration.ShipID = ship.Symbol;
+            ship.Nav.ShipID = ship.Symbol;
+            ship.Crew.ShipID = ship.Symbol;
+            ship.Frame.ShipID = ship.Symbol;
+            ship.Reactor.ShipID = ship.Symbol;
+            ship.Engine.ShipID = ship.Symbol;
+            foreach (var mod in ship.Modules) mod.ShipID = ship.Symbol;
+            foreach(var mount in ship.Mounts) mount.ShipID = ship.Symbol;
+            ship.Cargo.ShipID = ship.Symbol;
+            foreach(var cargo in ship.Cargo.Inventory) cargo.CargoID = ship.Symbol;
+            ship.Fuel.ShipID = ship.Symbol;
         }
+        await _connection.InsertOrReplaceAllWithChildrenAsync(ships, true);
     }
 
-    private async Task DeleteShipCargo (ShipData ship) => await _connection.Table<CargoItem>().DeleteAsync(x => x.Ship == ship.Symbol);
+    private async Task DeleteShipCargo (Ship ship)
+    {
+        await _connection.GetChildAsync(ship, "Cargo", true);
+        await _connection.DeleteAsync(ship.Cargo, true);
+    }
 
     public async Task DeleteAllFromAgent(Agent data)
     {
@@ -50,12 +55,8 @@ internal class ShipDbController : BaseDbController
         await _connection.DeleteAllAsync(allShips, true);
     }
 
-    public async Task<List<ShipData>> GetAgentShips (Agent data) => await GetAgentShips(data.AccountID);
-    public async Task<List<ShipData>> GetAgentShips (string accountID)
-    {
-        await Initialize();
-        return await _connection.GetAllWithChildrenAsync<ShipData>(ship => ship.AccountID == accountID);
-    }
+    public async Task<List<Ship>> GetAgentShips (Agent data) => await GetAgentShips(data.AccountID);
+    public async Task<List<Ship>> GetAgentShips (string accountID) => await _connection.GetAllWithChildrenAsync<Ship>(ship => ship.AccountID == accountID);
 }
 
 public static class SqliteExtensions
